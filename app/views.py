d9ffdb621aed316d32d9e4c17afade786b1acbb3
Valérie <samuelpilcer@gmail.com>
2018-01-12 16:50:28 +0100

    Test

--- app/views.py
+++ app/views.py
@@ -73,12 +73,14 @@ def model(request, id):
     if model.admin!=request.user:
         return redirect('home')
 
+    print("Back-end id : "+str(model.back_end_id))
     if model.back_end_id == 0:
         is_trained=False
     else:
         url = 'http://m-learning.fr:50/'+model.back_end_id
         r = requests.get(url, headers={'Token':'test_password_12345'}).json()["model"]["trained"]
         is_trained=r
+        print(r)
     #print(requests.get("http://m-learning.fr:50/1", headers={'Token':'test_password_12345'}).json()["model"]["trained"])
     models=[model]
     id_model=id
