02baea8c66d1492f05d575b305821a98b9aee0bc
Valérie <samuelpilcer@gmail.com>
2018-01-12 17:24:45 +0100

    New page

--- app/views.py
+++ app/views.py
@@ -17,6 +17,7 @@ import json
 import csv
 import pandas as pd
 import os
+from django.views.static import serve
 
 # Create your views here.
 
@@ -136,8 +137,9 @@ def process_model(request, id):
         frame=pd.read_csv(file)
         frame.to_csv('../MachineLearningAPI/static/to_process/file_'+str(model_ML.back_end_id)+'.csv', index=False)
         url_process = 'http://m-learning.fr:50/process_file/'+str(model_ML.back_end_id)
-        r_train = requests.post(url_train, headers={'Token':'test_password_12345', "Content-Type":"application/json"}, data=json.dumps({"description":"Test", "training_file":"static/training/training_3.csv", "training_columns":785, "output_column":786, "epochs":1}))
-        return model(request, id)
+        r_train = requests.post(url_train, headers={'Token':'test_password_12345', "Content-Type":"application/json"})
+        url_serve='../MachineLearningAPI/static/processed/file_'+str(model_ML.back_end_id)+'.csv'
+        return serve(request, os.path.basename(url_serve), os.path.dirname(url_serve))
     else:
         form = TrainingForm()
         return render(request, 'form.html', locals())
