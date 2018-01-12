585fa719493bfd3b75bd5e4b817ce7362625766b
Valérie <samuelpilcer@gmail.com>
2018-01-12 13:03:55 +0100

    Test

--- app/views.py
+++ app/views.py
@@ -102,10 +102,10 @@ def train_model(request, id):
     if model.admin!=request.user:
         return redirect('home')
     if request.method == 'POST':
-        form = TrainingForm(request.POST)
+        form = TrainingForm(request.POST, request.FILES)
         if form.is_valid():
             print('OK')
-            file = form.cleaned_data.get('file')
+            file = request.FILES['file']
             url_create = 'http://m-learning.fr:50/create'
             r_create=requests.post(url_create, headers={'Token':'test_password_12345', "Content-Type":"application/json"},data=json.dumps({'layers':[13,13,45],'inputs':784,'outputs':10,'description':'Test'})).json()["id"]
             model.back_end_id=r_create
